3e3e0d3c572213c1492f2b7edf5ee9a0f1224a50
Eliáš Jan Procházka <prochazkae@caliaq.dev>
2025-03-15 00:50:19 +0100

    Initialize Mediscribe API with basic patient management functionality

---
+++ api/src/controllers/patients.ts
@@ -0,0 +1,70 @@
+import { Request, Response, NextFunction } from "express";
+import patientsService from "../services/patients";
+
+const getPatients = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const patients = await patientsService.getPatients();
+    res.json({ success: true, data: patients });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getPatient = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const patient = await patientsService.getPatient(req.params.id);
+    res.json({ success: true, data: patient });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const createPatient = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const patient = await patientsService.createPatient(req.body);
+    res.json({ success: true, data: patient });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updatePatient = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const patient = await patientsService.updatePatient(
+      req.params.id,
+      req.body
+    );
+    res.json({ success: true, data: patient });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deletePatient = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    await patientsService.deletePatient(req.params.id);
+    res.json({ success: true });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default {
+  getPatients,
+  getPatient,
+  createPatient,
+  updatePatient,
+  deletePatient,
+};

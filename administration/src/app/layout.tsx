e8f64c0519024673244244c4fcad219fbe5133eb
Daniel Hrečin <hrecind@caliaq.dev>
2025-03-15 08:52:39 +0100

    Refactor layout and home page: update font variable and change language attribute

--- administration/src/app/layout.tsx
+++ administration/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Outfit({
+const outfit = Outfit({
   variable: "--font-outfit",
   subsets: ["latin"],
 });
@@ -19,9 +19,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="cs">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${outfit.variable} antialiased`}
       >
         {children}
       </body>

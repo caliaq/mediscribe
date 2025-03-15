dfdc10c9a5921edb09ce0bb18901c1219741a3ce
Eliáš Jan Procházka <prochazkae@caliaq.dev>
2025-03-15 21:33:08 +0100

    REFactor: Simplify express import statement in index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import { connect } from "mongoose";
 import dotenv from "dotenv";
 import path from "path";

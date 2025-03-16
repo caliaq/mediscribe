d9c7187bf8103d6420d505e7d1d6881398c7d770
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2025-03-16 06:15:02 +0100

    Revert "Refactor: Enhance PatientCard component with audio download functiona…"

--- api/src/models/record.ts
+++ api/src/models/record.ts
@@ -5,7 +5,7 @@ const recordSchema = new Schema(
     patientId: { type: Schema.Types.ObjectId, required: true },
     date: { type: Date, default: Date.now },
     data: { type: String, required: true },
-    filePath: { type: String, required: true },
+    summary: { type: String },
   },
   { versionKey: false }
 );
